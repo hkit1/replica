661354a0d7bf2214525f81efca023a93982ed4c4
Gureumi <cloud9350@naver.com>
2023-10-24 10:30:25 +0900

    파일 생성

---
+++ src/main/java/com/example/hkit1/Hkit1Application.java
@@ -0,0 +1,13 @@
+package com.example.hkit1;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Hkit1Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Hkit1Application.class, args);
+    }
+
+}
