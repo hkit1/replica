a3ac5ce37714ff735e3b5f7c8d3d512493755383
Gureumi <cloud9350@naver.com>
2023-10-24 12:02:17 +0900

    글쓰기 및 저장 구현

--- src/main/java/com/example/hkit/controller/PostController.java
+++ src/main/java/com/example/hkit/controller/PostController.java
@@ -1,16 +1,24 @@
 package com.example.hkit.controller;
 
 import com.example.hkit.dto.PostDTO;
+import com.example.hkit.entity.Post;
+import com.example.hkit.repository.PostRepository;
 import com.example.hkit.service.PostService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.Optional;
 
 @Controller
 @RequiredArgsConstructor
 public class PostController {
+    private final PostRepository postRepository;
+
     private final PostService postService;
 
     @GetMapping("/post")
@@ -22,5 +30,13 @@ public class PostController {
     public String post(@ModelAttribute PostDTO postDTO) {
         postService.save(postDTO);
         return "index";
+    }
+
+    @PostMapping("/search")
+    public String search(@RequestParam(name = "text") String text, Model model) {
+        // TODO: 2023-10-24 검색 만들기
+        Optional<Post> result = postRepository.findAllByTitleContains(text);
+
+        return "search";
     }
 }
--- src/main/java/com/example/hkit/repository/PostRepository.java
+++ src/main/java/com/example/hkit/repository/PostRepository.java
@@ -4,6 +4,9 @@ import com.example.hkit.entity.Post;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface PostRepository extends JpaRepository<Post, Long> {
+    Optional<Post> findAllByTitleContains(String text);
 }
