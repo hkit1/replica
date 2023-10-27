9cd92d1b7135ba8dfaa1bd218a0b8a0d92eb9fe3
Gureumi <cloud9350@naver.com>
2023-10-27 10:57:35 +0900

    REST API 구현

--- src/main/java/com/example/hkit/HkitApplication.java
+++ src/main/java/com/example/hkit/HkitApplication.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class HkitApplication {
-
     public static void main(String[] args) {
         SpringApplication.run(HkitApplication.class, args);
     }
